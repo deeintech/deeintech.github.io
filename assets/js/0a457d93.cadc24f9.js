4040476f1075d3cbeff6ae6f9d77407206a36686
markofdao <dmitrydaopro@gmail.com>
2023-06-15 14:18:47 +0700

    Deploy website - based on 669744a41f039c06986577177c72f4a4e29f9eee

--- assets/js/0a457d93.cadc24f9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdeeintech=self.webpackChunkdeeintech||[]).push([[378],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Dev Portals Research","permalink":"/blog/dev-portals-research"},{"title":"How to manage a VPS","permalink":"/blog/manage-vps"}]}')}}]);
---
+++ assets/js/ab6e588c.dee32b8e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdeeintech=self.webpackChunkdeeintech||[]).push([[879],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Dev Portals Research","permalink":"/blog/dev-portals-research"},{"title":"How to manage a VPS","permalink":"/blog/manage-vps"}]}')}}]);
