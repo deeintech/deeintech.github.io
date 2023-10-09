79da2196bfb3d8313847d5049848ff487aac41d1
markofdao <dmitrydaopro@gmail.com>
2023-10-09 14:45:57 +0700

    Deploy website - based on 5cd29e0a43eacd8afcccc7fa2f4898736a35c343

--- assets/js/9ccb5ae6.91b16a53.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdeeintech=self.webpackChunkdeeintech||[]).push([[113],{6738:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"tech-writing","name":"tech writing","count":2,"permalink":"/blog/tags/tech-writing"}')}}]);
--- assets/js/b2b675dd.120037a2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdeeintech=self.webpackChunkdeeintech||[]).push([[533],{8017:function(e){e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":1000,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
